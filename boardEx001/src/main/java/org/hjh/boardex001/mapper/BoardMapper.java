426764cdc1bc8d527bbfc732c6127f82bdf420f8
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-09-10 17:32:36 +0900

    End class8
    *Spring
    
    -oracle db conect

--- boardEx001/src/main/java/org/hjh/boardex001/mapper/BoardMapper.java
+++ boardEx001/src/main/java/org/hjh/boardex001/mapper/BoardMapper.java
@@ -2,11 +2,15 @@ package org.hjh.boardex001.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.hjh.boardex001.domain.BoardVo;
 
 public interface BoardMapper {
 	
 	@Select("select * from board where bno > 0")
+//	@Delete("delete from board where bno = 2")
+//	@Insert("insert into board values")
 	public List<BoardVo> getList();
 }
