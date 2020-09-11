3e08c803c3880355ef364b33a2104ce4671a0825
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-09-11 10:50:38 +0900

    End class2
    *Spring
    
    - mybatis mapping으로 select,insert

--- boardEx001/src/main/java/org/hjh/boardex001/mapper/BoardMapper.java
+++ boardEx001/src/main/java/org/hjh/boardex001/mapper/BoardMapper.java
@@ -9,8 +9,16 @@ import org.hjh.boardex001.domain.BoardVo;
 
 public interface BoardMapper {
 	
-	@Select("select * from board where bno > 0")
+	//짧은 쿼리 이용시, 긴쿼리 = mybatis 매핑
+	//@Select("select * from board where bno > 0")
 //	@Delete("delete from board where bno = 2")
 //	@Insert("insert into board values")
 	public List<BoardVo> getList();
+	public void insert(BoardVo board);
+	
+	//시퀀스로 no 생성시, insert하기 전 no값을 얻고 싶을때 사용. 
+	public void insertSelectKey(BoardVo board);
+	
+	public List<BoardVo> selectNo(int n);
+	
 }
--- boardEx001/src/test/java/org/hjh/boardex001/BoardMapperTest.java
+++ boardEx001/src/test/java/org/hjh/boardex001/BoardMapperTest.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.hjh.boardex001.domain.BoardVo;
 import org.hjh.boardex001.mapper.BoardMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,6 +26,7 @@ public class BoardMapperTest {
 	
 	@Setter(onMethod_ = @Autowired)
 	private DataSource dataSource;
+	//getBean();안해도 되는 이유 = web.xml에 경로 설정되어있음.
 	
 	@Test
 	public void testConnection() {
@@ -36,8 +38,33 @@ public class BoardMapperTest {
 	}
 	
 	@Test
-	public void testGList() {
+	public void testList() {
 		mapper.getList().forEach(board->log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVo board = new BoardVo();
+		board.setTitle("new title");
+		board.setContent("new content");
+		board.setWriter("new writer");
+		mapper.insert(board);
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVo board = new BoardVo();
+		board.setTitle("new title2");
+		board.setContent("new content2");
+		board.setWriter("new writer2");
+		mapper.insert(board);
+		log.info(board);
+	}
+	
+	@Test
+	public void testListNo() {
+		mapper.selectNo(4).forEach(board->log.info(board));
 	}
 	
 }
